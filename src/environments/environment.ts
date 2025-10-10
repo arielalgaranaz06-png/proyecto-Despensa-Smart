0f6fc4e05f7d7a03c09452f4bd95f5eec4c78986
Josue <vargaspantojajosue@gmail.com>
2025-10-10 01:30:55 -0400

    testDB

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,22 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+  apiKey: "AIzaSyCDnA1MD7U_-BDhUkZOe3ywy4nVHzn_rII",
+  authDomain: "proyecto-despensa-smart.firebaseapp.com",
+  projectId: "proyecto-despensa-smart",
+  storageBucket: "proyecto-despensa-smart.firebasestorage.app",
+  messagingSenderId: "132160270331",
+  appId: "1:132160270331:web:64dcc40f1d3b352e85a55e"
+}
 };
+
+/*import { initializeApp } from "firebase/app";*/
+
+
+
+/*const app = initializeApp(firebaseConfig);*/
 
 /*
  * For easier debugging in development mode, you can import the following file
